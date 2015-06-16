21ea319f697dba67ce101d40ea04ce4d6e3800b2
Jacy Wang <jacywj@gmail.com>
2015-06-15 19:05:58 -0700

    Add WelcomeViewController

---
+++ ValueFaces/WelcomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ValueFaces
+//
+//  Created by JIAN WANG on 6/15/15.
+//  Copyright © 2015 JWANG. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var goButton: UIButton!
+    
+    let buttonBorderWith: CGFloat = 2.0
+    let cornerRadiusDivider: CGFloat = 2.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        goButton.layer.borderWidth = buttonBorderWith
+        goButton.layer.borderColor = UIColor.whiteColor().CGColor
+        goButton.layer.cornerRadius = goButton.frame.size.height / cornerRadiusDivider
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
